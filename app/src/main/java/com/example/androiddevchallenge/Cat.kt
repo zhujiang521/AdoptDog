2080f38a98ebb58fe0886298b6b604e672ebaf70
zhujiang2 <zhujiang2@lenovo.com>
2021-03-01 17:02:30 +0800

    Add new dog.

---
+++ app/src/main/java/com/example/androiddevchallenge/Cat.kt
@@ -0,0 +1,7 @@
+package com.example.androiddevchallenge
+
+data class Cat(
+    val name: String = "Default Dog",
+    val photo: Int = R.drawable.dog0,
+    val desc: String = ""
+)
--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -15,15 +15,27 @@
  */
 package com.example.androiddevchallenge
 
+import android.content.Context
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.androiddevchallenge.ui.theme.MyTheme
 
 class MainActivity : AppCompatActivity() {
@@ -31,7 +43,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MyTheme {
-                MyApp()
+                MyApp(this)
             }
         }
     }
@@ -39,9 +51,49 @@ class MainActivity : AppCompatActivity() {
 
 // Start building your app here!
 @Composable
-fun MyApp() {
+fun MyApp(context: Context) {
+    val catList = arrayListOf<Cat>()
+    catList.apply {
+        add(Cat("Jack", R.drawable.dog0))
+        add(Cat("Danny", R.drawable.dog1))
+        add(Cat("Lucy", R.drawable.dog2))
+        add(Cat("Make", R.drawable.dog3))
+        add(Cat("Zhu", R.drawable.dog4))
+        add(Cat("Juu", R.drawable.dog5))
+        add(Cat("Tue", R.drawable.dog6))
+        add(Cat("Jia", R.drawable.dog7))
+        add(Cat("Cake", R.drawable.dog8))
+        add(Cat("Luci", R.drawable.dog9))
+    }
     Surface(color = MaterialTheme.colors.background) {
-        Text(text = "Ready... Set... GO!")
+        LazyColumn(content = {
+            items(catList) { item ->
+                //列表内容
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(10.dp)
+                        .clickable {
+                            Toast
+                                .makeText(
+                                    context,
+                                    "Congratulations on your successful adoption : ${item.name} !",
+                                    Toast.LENGTH_SHORT
+                                )
+                                .show()
+                        },
+                    horizontalArrangement = Arrangement.SpaceAround,
+                    verticalAlignment = Alignment.CenterVertically,
+                ) {
+                    Image(
+                        painter = painterResource(id = item.photo),
+                        contentDescription = "",
+                        modifier = Modifier.size(100.dp)
+                    )
+                    Text(text = "Dog name：${item.name}")
+                }
+            }
+        })
     }
 }
 
@@ -49,14 +101,6 @@ fun MyApp() {
 @Composable
 fun LightPreview() {
     MyTheme {
-        MyApp()
-    }
-}
-
-@Preview("Dark Theme", widthDp = 360, heightDp = 640)
-@Composable
-fun DarkPreview() {
-    MyTheme(darkTheme = true) {
-        MyApp()
+        //MyApp()
     }
 }
